3a254c1eeb4fbc510737f79a8c06866b03f835ab
rahul saini <sainisab454501072004@gmail.com>
2024-07-05 12:57:28 +0530

    FORM Submit

---
+++ src/Form.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+
+const Form = () => {
+  const [name, setvalue] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [course, setCourse] = useState("");
+  const [Age, setAge] = useState("");
+
+  const [alldata, setalldata] = useState([]);
+
+  const getuser = (e) => {
+    e.preventDefault();
+    const show = {
+      name,
+      email,
+      password,
+      course,
+      Age,
+    };
+    const newalldata = alldata.concat(show);
+    setalldata(newalldata);
+  };
+
+  return (
+    <div className="text-center d-flex justify-content-around   bg-success me-5 ms-5 mt-5 border-denger ">
+      <form
+        onSubmit={(e) => {
+          getuser(e.target.value);
+        }}
+        className="mt-5"
+      >
+        <h2>FORM</h2>
+
+        <label className="text-center input-group-text">
+          Name:
+          <input
+            onChange={(e) => {
+              setvalue(e.target.value);
+            }}
+            type="text"
+            name="name"
+          />
+        </label>
+        <br />
+
+        <label className="text-center input-group-text d-flex justify-content-around">
+          Email:
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            name="email"
+          />
+        </label>
+        <br />
+
+        <label className="text-center input-group-text">
+          Password:
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            name="password"
+          />
+        </label>
+        <br />
+
+        <label className="text-center input-group-text">
+          course
+          <input
+            onChange={(e) => {
+              setCourse(e.target.value);
+            }}
+            type="text"
+            name="course"
+          />
+        </label>
+        <br />
+
+        <label className="text-center input-group-text">
+          Age
+          <input
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+            type="text"
+            name="course"
+          />
+        </label>
+        <br />
+
+        <button
+          onClick={getuser}
+          className="btn btn-danger"
+          type="submit"
+          value="Submit"
+        >
+          Submit
+        </button>
+        <br />
+        <br />
+      </form>
+
+      {alldata?.map((i) => {
+        return (
+          <div className="mt-5 ms-5 me-5 bg-danger">
+            <h2>{i.name}</h2>
+            <h2>{i.email}</h2>
+            <h2>{i.password}</h2>
+            <h2>{i.course}</h2>
+            <h2>{i.Age}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Form;
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+// import React, { useState } from 'react'
+
+// const Home = () => {
+//   const [value,setValue] = useState(0);
+//   // const check = ()=>{
+//   //   if(value>0){
+//   //     setValue(value-1)
+//   //   }
+//   // }
+//   return (
+//     <div>
+//       <h1>{value}</h1>
+//       <button onClick={()=>setValue(value+1)} >increment</button>
+//       <button onClick={()=>value>0?setValue(value-1):0} >decrement</button>
+//     </div>
+//   )
+
+// }
+// export default Home;
